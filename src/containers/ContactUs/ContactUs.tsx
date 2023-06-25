16e1e0c14035cd5d664407807ed21545ce6536f1
Dmitrii Morrows <trollman1993@gmail.com>
2023-06-25 18:20:47 +0600

    добавлен пункт навигации contact us и контейнер для него

--- src/containers/ContactUs/ContactUs.tsx
+++ src/containers/ContactUs/ContactUs.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import './ContactUs.css';
+
 const ContactUs = () => {
   return (
-    <div className="container" style={{paddingTop: '150px'}}>
-      contact us
+    <div className="contact__wrap">
+      <div className="container" style={{paddingTop: '150px'}}>
+        <div className="form__wrap">
+          <h3 className="form__title mb">Contact Us</h3>
+
+          <form className="form" onSubmit={e => e.preventDefault()}>
+            <div className="mb">
+              <input type="text" placeholder="name" name="name" />
+            </div>
+
+            <div className="mb">
+              <input type="email" placeholder="Email" name="email" />
+            </div>
+
+            <div className="mb">
+              <textarea placeholder="Write something..." name="message" />
+            </div>
+
+            <button>Submit</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
